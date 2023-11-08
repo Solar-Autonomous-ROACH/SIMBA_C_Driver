c9a8a1d3a02bc400c598bc071763a877cb5f16c8
ubuntu <ubuntu@simba.rover>
2023-11-08 19:03:53 +0000

    updated for encoder interfacing

--- test/main.c
+++ test/main.c
@@ -30,24 +30,31 @@ int main() {
   uint8_t duty_cycle = 0;  // 8 bits
   uint8_t clk_divisor = 4; // 3 bits
   uint8_t dir = 1;         // 1 bit
-  uint8_t en = 1;          // 1 bit
+  uint8_t en_motor = 1;    // 1 bit
+  uint8_t clear_enc = 0;   // 1 bit
+  uint8_t en_enc = 1;      // 1 bit
 
-  *mmio = duty_cycle + (clk_divisor << 8) + (dir << 11) + (en << 12);
+  *mmio = duty_cycle + (clk_divisor << 8) + (dir << 11) + (en_motor << 12) +
+          (clear_enc << 13) + (en_enc << 14);
 
   while (!done) {
     for (duty_cycle = 0; duty_cycle < 255 && !done; duty_cycle++) {
       usleep(50000);
-      *mmio = duty_cycle + (clk_divisor << 8) + (dir << 11) + (en << 12);
+      *mmio = duty_cycle + (clk_divisor << 8) + (dir << 11) + (en_motor << 12) +
+              (clear_enc << 13) + (en_enc << 14);
+      printf("enc: %d\n", *mmio);
     }
     for (duty_cycle = 255; duty_cycle > 0 && !done; duty_cycle--) {
       usleep(50000);
-      *mmio = duty_cycle + (clk_divisor << 8) + (dir << 11) + (en << 12);
+      *mmio = duty_cycle + (clk_divisor << 8) + (dir << 11) + (en_motor << 12) +
+              (clear_enc << 13) + (en_enc << 14);
     }
   }
 
-  en = 0;
+  en_motor = 0;
 
-  *mmio = duty_cycle + (clk_divisor << 8) + (dir << 11) + (en << 12);
+  *mmio = duty_cycle + (clk_divisor << 8) + (dir << 11) + (en_motor << 12) +
+          (clear_enc << 13) + (en_enc << 14);
 
   printf("closing mmio\n");
   close_mem();
